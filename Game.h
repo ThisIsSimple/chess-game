eaffeea5c82fb8c9c4ca8d45b5042354cb2e06d2
전윤민 <cordelia273@naver.com>
2021-07-12 23:25:54 +0900

    update namespace

--- Game.h
+++ Game.h
@@ -2,14 +2,11 @@
 
 namespace chess
 {
-	namespace answer
-	{
-		void MainMenu(std::istream& is, std::ostream& os);
-		
-		void PrintInitialBoard(std::ostream& os);
-		
-		void LoadBoard(std::istream& is, std::ostream& os);
-		
-		void PrintGameInformation(std::istream& is, std::ostream& os);
-	}
+	void MainMenu(std::istream &is, std::ostream &os);
+
+	void PrintInitialBoard(std::ostream &os);
+
+	void LoadBoard(std::istream &is, std::ostream &os);
+
+	void PrintGameInformation(std::istream &is, std::ostream &os);
 }
